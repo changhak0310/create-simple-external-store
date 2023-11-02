73bc487405df47fbde40d7690ca7b34894cffafd
changhak0310 <dlckdgkr0320@naver.com>
2023-11-03 01:04:51 +0900

    아키텍쳐 리팩토링

--- src/stores/store1.tsx
+++ src/stores/store1.tsx
@@ -28,39 +28,8 @@ const CountPlus = () => {
     store.setState((prev) => ({ ...prev, count: prev.count + 1 }));
 }
 
-// const Counter = () => {
-//     const count = useStore(
-//       store,
-//       useCallback((state) => state.count, [])
-//     );
-//     const inc = () => {
-//       store.setState((prev) => ({ ...prev, count: prev.count + 1 }));
-//     };
-//     return (
-//       <div>
-//         {count} <button onClick={inc}>+1</button>
-//       </div>
-//     );
-//   };
-
 const ChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
   store.setState((prev) => ({ ...prev, text: event.target.value }));
 };
-
-// const TextBox = () => {
-//     const text = useStore(
-//       store,
-//       useCallback((state) => state.text, [])
-//     );
-//     const setText = (event: React.ChangeEvent<HTMLInputElement>) => {
-//       store.setState((prev) => ({ ...prev, text: event.target.value }));
-//     };
-//     return (
-//       <div>
-//         <input value={text} onChange={setText} className="full-width" />
-//       </div>
-//     );
-//   };
   
-
 export { GetCount, GetText, CountPlus, ChangeText };
